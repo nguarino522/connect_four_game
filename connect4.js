7b14be1bea01032df54c7e882b073752a0ab4429
Nicholas Guarino <nguarino@datto.lan>
2022-08-29 16:56:12 -0400

    minor update create board done

--- connect4.js
+++ connect4.js
@@ -10,20 +10,21 @@ let defaultWidth = 7;
 let defaultHeight = 6;
 
 let currPlayer = 1; // active player: 1 or 2
-const board = []; // array of rows, each row is array of cells  (board[y][x])
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
 */
 
 function makeBoard() {
-  // TODO: set "board" to empty defaultHeight x defaultWidth matrix array
+  // set "board" to empty defaultHeight x defaultWidth matrix array
+  board = [...Array(defaultHeight)].map(e => Array(defaultWidth));
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  // get "htmlBoard" variable from the item in HTML w/ID of "board"
   let htmlBoard = document.getElementById("board");
 
   // TODO: add comment for this code
@@ -39,9 +40,9 @@ function makeHtmlBoard() {
   htmlBoard.append(top);
 
   // TODO: add comment for this code
-  for (var y = 0; y < defaultHeight; y++) {
+  for (let y = 0; y < defaultHeight; y++) {
     const row = document.createElement("tr");
-    for (var x = 0; x < defaultWidth; x++) {
+    for (let x = 0; x < defaultWidth; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
@@ -73,10 +74,10 @@ function endGame(msg) {
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  let x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
   if (y === null) {
     return;
   }
@@ -117,12 +118,12 @@ function checkForWin() {
 
   // TODO: read and understand this code. Add comments to help you.
 
-  for (var y = 0; y < defaultHeight; y++) {
-    for (var x = 0; x < defaultWidth; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  for (let y = 0; y < defaultHeight; y++) {
+    for (let x = 0; x < defaultWidth; x++) {
+      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      let diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
@@ -133,3 +134,4 @@ function checkForWin() {
 
 makeBoard();
 makeHtmlBoard();
+
